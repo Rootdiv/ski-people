5e78ecf5a3c5d9dee87c5e7cbb228d179e8f8531
Rootdiv <rootdiv9152@gmail.com>
2025-04-04 13:35:57 +0300

    feat: add the swiper slider

--- src/main.js
+++ src/main.js
@@ -1,2 +1,24 @@
 import 'normalize.css';
 import './style.scss';
+
+import { Swiper } from 'swiper';
+import { Navigation, Thumbs } from 'swiper/modules';
+import 'swiper/css';
+
+const thumbnailsSwiper = new Swiper('.product__slider-thumbnails', {
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+new Swiper('.product__slider', {
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.product__slider-arrow_next',
+    prevEl: '.product__slider-arrow_prev',
+  },
+  modules: [Navigation, Thumbs],
+  thumbs: {
+    swiper: thumbnailsSwiper,
+  },
+});
